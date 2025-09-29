2da7ebe3b6764db7eb0a67d227030e96b2ada9b2
weroz07 <daniilparakhin2007@gmail.com>
2025-09-30 00:05:11 +0300

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,19 @@
+//№3
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_CTYPE, "RUS");
+    int n = 3;
+    int L = 2333;
+    float res = (float)n / L;
+
+    printf("Дано:\n");
+    printf("%13d\n", n);
+    printf("%13d\n", L);
+    printf("--------------------\n");
+    printf("Результат:\n");
+    printf("%8c %+010.5f\n", ' ', res);
+
+    return 0;
+}
